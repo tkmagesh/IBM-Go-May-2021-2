2b07a31f9c77c1219b38016e6fcb082cad4c1468
Magesh Kuppan <tkmagesh77@gmail.com>
2021-06-01 17:08:21 +0530

    fibonacci series - assignment - solution

--- 10-concurrency/10-demo.go
+++ 10-concurrency/10-demo.go
@@ -1,10 +1,22 @@
 package main
 
+import "fmt"
+
 func main() {
-	go fibonacci(20 /*  */)
-	//print the fibonacci numbers
+	ch := make(chan int, 1)
+	go fibonacci(20, ch)
+	for no := range ch {
+		fmt.Println(no)
+	}
+	fmt.Println("Done")
 }
 
-func fibonacci(count int /*  */) {
+func fibonacci(count int, ch chan int) {
 	//generate the fibonacci numbers
+	x, y := 0, 1
+	for i := 0; i < count; i++ {
+		ch <- x
+		x, y = y, x+y
+	}
+	close(ch)
 }
