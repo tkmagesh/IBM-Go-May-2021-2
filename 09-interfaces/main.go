da84cbb67f3c75e87170f4d26cc63cb8e4d3e0ef
Magesh Kuppan <tkmagesh77@gmail.com>
2021-06-01 12:26:06 +0530

    Update main.go

--- 09-interfaces/main.go
+++ 09-interfaces/main.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+type User struct {
+	name string
+}
+
 func main() {
 	var x interface{}
 	x = 10
@@ -16,12 +20,15 @@ func main() {
 		fmt.Println("value cannot be converted to int")
 	}
 
-	x = "Magesh"
+	//x = "Magesh"
+	x = User{"Magesh"}
 	switch val := x.(type) {
 	case int:
 		fmt.Printf("Twice of %v is %v\n", val, 2*val)
 	case string:
 		fmt.Printf("Length of the string is %v\n", len(val))
+	case User:
+		fmt.Println("User name is ", val.name)
 	default:
 		fmt.Println("Unknown type")
 	}
