a39156c9207e344483766b542c8ca9d1d094003b
Magesh Kuppan <tkmagesh77@gmail.com>
2021-06-01 12:55:55 +0530

    assignment solution

--- 09-interfaces/sum.go
+++ 09-interfaces/sum.go
@@ -1,29 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 	fmt.Println("Generic Sum fn")
-	fmt.Println(sum(10, 20))         //=> 30
-	fmt.Println(sum(10, 20, 30, 40)) //=> 100
-	fmt.Println(sum(10))             //=> 10
-	fmt.Println(sum())               //=> 0
-
-	nos := []int{10, 20, 30, 40}
-	fmt.Println(sum(nos...))
-	/*
-		fmt.Println(sum(10, "20", 30, "40"))                                     //=> 100
-		fmt.Println(sum(10, "20", 30, "40", "abc"))                              //=> 100
-		fmt.Println(sum(10, 20, []int{30, 40}))                                  //=> 100
-		fmt.Println(sum(10, 20, []interface{}{30, 40, []int{10, 20}}))           //=> 130
-		fmt.Println(sum(10, 20, []interface{}{30, 40, []interface{}{10, "20"}})) //=> 130
-	*/
+	fmt.Println(sum(10, 20))                                                 //=> 30
+	fmt.Println(sum(10, 20, 30, 40))                                         //=> 100
+	fmt.Println(sum(10))                                                     //=> 10
+	fmt.Println(sum())                                                       //=> 0
+	fmt.Println(sum(10, "20", 30, "40"))                                     //=> 100
+	fmt.Println(sum(10, "20", 30, "40", "abc"))                              //=> 100
+	fmt.Println(sum(10, 20, []int{30, 40}))                                  //=> 100
+	fmt.Println(sum(10, 20, []interface{}{30, 40, []int{10, 20}}))           //=> 130
+	fmt.Println(sum(10, 20, []interface{}{30, 40, []interface{}{10, "20"}})) //=> 130
 }
 
-func sum(nos ...int) int {
+func sum(nos ...interface{}) int {
 	result := 0
-	for _, no := range nos {
-		result += no
+	for _, val := range nos {
+		switch val.(type) {
+		case int:
+			result += val.(int)
+		case string:
+			if x, err := strconv.Atoi(val.(string)); err != nil {
+				result += x
+			}
+		case []int:
+			values := val.([]int)
+			intValList := make([]interface{}, len(values))
+			for idx, x := range values {
+				intValList[idx] = x
+			}
+			result += sum(intValList...)
+		case []interface{}:
+			result += sum(val.([]interface{})...)
+		}
 	}
 	return result
 }
