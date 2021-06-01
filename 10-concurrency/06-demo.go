f9e45a6163ad66be23f0ae5445c8d64a76abbaa7
Magesh Kuppan <tkmagesh77@gmail.com>
2021-06-01 15:36:38 +0530

    assignment solution

--- 10-concurrency/06-demo.go
+++ 10-concurrency/06-demo.go
@@ -5,17 +5,31 @@ import (
 	"time"
 )
 
-func print(msg string, delay time.Duration) {
+func print(msg string, delay time.Duration, ch1 chan string, ch2 chan string) {
 	for i := 0; i < 5; i++ {
+		<-ch1
 		time.Sleep(delay)
 		fmt.Println(msg)
+		ch2 <- "done"
 	}
 }
 
 func main() {
-	print("Hello", 2*time.Second)
-	print("World", 3*time.Second)
+	ch1, ch2 := make(chan string), make(chan string)
+	go print("Hello", 1*time.Second, ch1, ch2)
+	go print("World", 1*time.Second, ch2, ch1)
+	ch1 <- "Start"
 	fmt.Println("press ENTER to exit..")
 	var input string
 	fmt.Scanln(&input)
 }
+
+/*
+Desired Output:
+Hello
+World
+Hello
+World
+...
+
+*/
